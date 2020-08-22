0bb2faf110890955a456a6aaada169147b96d0f2
lykis <lykismekk@outlook.com>
2020-08-22 10:58:30 +0800

    Initial commit

---
+++ src/main/java/com/sia/tacocloud/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.sia.tacocloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")    // 控制器注解
+    public String home() { // 处理对跟路径"/"的请求
+        return "home";  // 返回视图名
+    }
+}
